ee7f6af034b6b2d14402e0c4c21dba8e8460dee0
Erick Gabriel G <87400941+erickdoubleg@users.noreply.github.com>
2021-10-14 22:58:57 -0500

    Add files via upload

---
+++ actividad17.m
@@ -0,0 +1,36 @@
+% Octave Script
+% Title            :Actividad 17
+% Authors          :Erick Gabriel Garcia
+% Date             :202123419
+% Version          :1
+% Usage			       :octave> /path/actividad17
+% Notes			       :Se requiere aplicacion Octave
+
+clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
