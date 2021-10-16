792e7c7f6fca65efafbd73bd5704dfcdc7953476
Erick Gabriel G <87400941+erickdoubleg@users.noreply.github.com>
2021-10-15 23:07:48 -0500

    Update actividad17.m

--- actividad17.m
+++ actividad17.m
@@ -1,12 +1,31 @@
 % Octave Script
-% Title            :Actividad 17
+% Title            :Funcion minimo material
+% Descrption       :Script para graficar una funcion de envase cilindrico
 % Authors          :Erick Gabriel Garcia
 % Date             :202123419
 % Version          :1
-% Usage			       :octave> /path/actividad17
+% Usage			       :octave> /path/enviar
 % Notes			       :Se requiere aplicacion Octave
 
-clear
+%limpiar varaibles 
+ clear
+%Ejemplo minimo de material para envase cilindrico
+%Rango de 0..1 en i=0.1
+ r=0:0.1:1;
+%Valor de la funsion 
+ ar=0(r) (pi() * (r.*2)) * (2./(r));
+%funcion a plotear
+ y=(pi() * (r.*2)) + (2./(r));
+%funsion para determinar valor minimo
+resr = fminbnd(ar,0,1);
+%Dibujar
+ plot(r, y);
+%Titulo
+ title(['Minimo material r = ' num2str(resr)]);
+%Etiqueta para x
+ xlabel(['Minimo r = ' num2str(resr)]);
+%Etiqueta para y
+ ylabel(['Min y = ' num2str( ar(resr))]);
 
 
 
